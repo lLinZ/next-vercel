807ae84f6eff7d06983423cc419863ad3e2abffa
Jose Linares <linz.webdev@gmail.com>
2022-03-07 10:58:42 -0400

    pre-deployment vercel

---
+++ components/layouts/DarkLayout.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react"
+
+export const DarkLayout: FC = ({ children }) => {
+    return (
+        <div style={{
+            backgroundColor: 'rgba(0,0,0,0.3)',
+            padding: '10px',
+            borderRadius: '5px'
+        }}>
+            <h3>DarkLayout</h3>
+            <div>
+                {children}
+            </div>
+        </div>
+    )
+}
